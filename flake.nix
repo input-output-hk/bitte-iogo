26a59d799129a85fa0a91f1180d78b496f42b755
David Arnold <david.arnold@iohk.io>
2022-04-13 15:48:40 -0500

    fix(arch): support darwin

--- flake.nix
+++ flake.nix
@@ -10,7 +10,13 @@
 
   outputs = { self, nixpkgs, utils, devshell, ... }@inputs:
     utils.lib.simpleFlake {
-      systems = [ "x86_64-linux" ];
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
       inherit nixpkgs;
 
       preOverlays = [ devshell.overlay ];
